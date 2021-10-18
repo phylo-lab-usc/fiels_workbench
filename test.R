7b4e24c7e03d9c05ba9cd857ec25927954f811bf
fieldima <60451947+fieldima@users.noreply.github.com>
2021-10-18 00:53:22 -0700

    Made a testing script

---
+++ test.R
@@ -0,0 +1,15 @@
+#Test script
+
+library(geiger)
+library(arbutus)
+
+geo <- finch$phy
+dat <- finch$data[,"wingL"]
+
+first_sim <- sim.char(geo, 0.02, 1000)
+
+unit_tree <- make_unit_tree(geo, data = dat)
+
+first_sim_unit <- simulate_char_unit(unit_tree)
+
+test.stat <- calculate_pic_stat(unit_tree)
