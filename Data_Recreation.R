fd89a4899e9362de6f193c39dec8a45102567655
fieldima <60451947+fieldima@users.noreply.github.com>
2021-10-25 20:37:23 -0700

    README update and lots of work on Data Recreation

--- Data_Recreation.R
+++ Data_Recreation.R
@@ -4,6 +4,8 @@
 library(geiger)
 library(arbutus)
 library(ape)
+library(tidyverse)
+library(OUwie)
 
 #Table 1 data
 
@@ -35,4 +37,73 @@ randplot <- plot(multirand, layout = 4)
 
 #Second step: divide selective regime
 
-#star trees were split between regime 1 and 2
+#For this step, need to make trait dataframes for each phylo object of format 1 species name and 2 selective regime
+
+#star genus and species
+star32 <- data.frame(multistar[[1]]$tip.label) %>% rename(Genus_species = multistar..1...tip.label)
+star64 <- data.frame(multistar[[2]]$tip.label) %>% rename(Genus_species = multistar..2...tip.label)
+star128 <- data.frame(multistar[[3]]$tip.label) %>% rename(Genus_species = multistar..3...tip.label)
+star512 <- data.frame(multistar[[4]]$tip.label) %>% rename(Genus_species = multistar..4...tip.label)
+
+#star selective regimes
+foo <- c(1:32)
+star32 <- star32 %>% 
+  mutate(n = foo) %>%
+  mutate(Reg = ifelse(n > 16, 2, 1)) %>%
+  select(Genus_species, Reg)
+
+foo <- c(1:64)
+star64 <- star64 %>% 
+  mutate(n = foo) %>%
+  mutate(Reg = ifelse(n > 32, 2, 1)) %>%
+  select(Genus_species, Reg)
+
+foo <- c(1:128)
+star128 <- star128 %>% 
+  mutate(n = foo) %>%
+  mutate(Reg = ifelse(n > 64, 2, 1)) %>%
+  select(Genus_species, Reg)
+
+foo <- c(1:512)
+star512 <- star512 %>% 
+  mutate(n = foo) %>%
+  mutate(Reg = ifelse(n > 256, 2, 1)) %>%
+  select(Genus_species, Reg)
+
+#edgelengths
+multistar[[1]] <- compute.brlen(multistar[[1]])
+multistar[[2]] <- compute.brlen(multistar[[2]])
+multistar[[3]] <- compute.brlen(multistar[[3]])
+multistar[[4]] <- compute.brlen(multistar[[4]])
+
+multibal[[1]] <- compute.brlen(multibal[[1]])
+
+#Nodes
+multistar[[1]] <- makeNodeLabel(multistar[[1]], method = "number", prefix = "")
+multistar[[2]] <- makeNodeLabel(multistar[[2]], method = "number", prefix = "")
+multistar[[3]] <- makeNodeLabel(multistar[[3]], method = "number", prefix = "")
+multistar[[4]] <- makeNodeLabel(multistar[[4]], method = "number", prefix = "")
+
+#Simulate OU models on star topologies
+#OUMV
+star32_MV <- OUwie.sim(multistar[[1]], star32, alpha = c(1.0,1.0), sigma.sq = c(0.45, 0.90), theta0 = 1.0, theta = c(1.0,2.0))
+star64_MV <- OUwie.sim(multistar[[2]], star64, alpha = c(1.0,1.0), sigma.sq = c(0.45, 0.90), theta0 = 1.0, theta = c(1.0,2.0))
+star128_MV <- OUwie.sim(multistar[[3]], star128, alpha = c(1.0,1.0), sigma.sq = c(0.45, 0.90), theta0 = 1.0, theta = c(1.0,2.0))
+star512_MV <- OUwie.sim(multistar[[4]], star512, alpha = c(1.0,1.0), sigma.sq = c(0.45, 0.90), theta0 = 1.0, theta = c(1.0,2.0))
+
+#OUMA
+star32_MA <- OUwie.sim(multistar[[1]], star32, alpha = c(1.0,0.5), sigma.sq = c(0.45, 0.45), theta0 = 1.0, theta = c(1.0,2.0))
+star64_MA <- OUwie.sim(multistar[[2]], star64, alpha = c(1.0,0.5), sigma.sq = c(0.45, 0.45), theta0 = 1.0, theta = c(1.0,2.0))
+star128_MA <- OUwie.sim(multistar[[3]], star128, alpha = c(1.0,0.5), sigma.sq = c(0.45, 0.45), theta0 = 1.0, theta = c(1.0,2.0))
+star512_MA <- OUwie.sim(multistar[[4]], star512, alpha = c(1.0,0.5), sigma.sq = c(0.45, 0.45), theta0 = 1.0, theta = c(1.0,2.0))
+
+#OUMVA
+star32_MVA <- OUwie.sim(multistar[[1]], star32, alpha = c(1.0,0.5), sigma.sq = c(0.45, 0.90), theta0 = 1.0, theta = c(1.0,2.0))
+star64_MVA <- OUwie.sim(multistar[[2]], star64, alpha = c(1.0,0.5), sigma.sq = c(0.45, 0.90), theta0 = 1.0, theta = c(1.0,2.0))
+star128_MVA <- OUwie.sim(multistar[[3]], star128, alpha = c(1.0,0.5), sigma.sq = c(0.45, 0.90), theta0 = 1.0, theta = c(1.0,2.0))
+star512_MVA <- OUwie.sim(multistar[[4]], star512, alpha = c(1.0,0.5), sigma.sq = c(0.45, 0.90), theta0 = 1.0, theta = c(1.0,2.0))
+
+
+#Fit corresponding model
+#OUMV OUwie fits
+OUwie(phy = multibal[[1]], data = star32_MV, model = "OUMV")
