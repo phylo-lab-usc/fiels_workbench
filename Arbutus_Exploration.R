aa2cd8fdbd899d88011e65c6779dd48691c0c461
fieldima <60451947+fieldima@users.noreply.github.com>
2021-10-26 13:33:16 -0700

    Ran simulations and fit adequacy models

--- Arbutus_Exploration.R
+++ Arbutus_Exploration.R
@@ -32,3 +32,41 @@ df_MVA <- OUwie.sim(tr, df, alpha = c(1.0, 0.5), sigma.sq = c(0.45, 0.9), theta0
 #Now to pivot data for use in fitting, need to fit data according to dat format, pull format from below
 geo <- get(data("geospiza"))
 geo$dat
+
+#Fixing dfs
+
+df_MV_fix <- df_MV
+row.names(df_MV_fix) <- df_MV$Genus_species
+df_MV_fix <- df_MV_fix %>% select(X)
+
+df_MA_fix <- df_MA
+row.names(df_MA_fix) <- df_MA$Genus_species
+df_MA_fix <- df_MA_fix %>% select(X)
+
+df_MVA_fix <- df_MVA
+row.names(df_MVA_fix) <- df_MVA$Genus_species
+df_MVA_fix <- df_MVA_fix %>% select(X)
+
+#Now fit a simple OU model to each OU type
+
+fit_MV <- fitContinuous(tr, df_MV_fix, model = "OU")
+fit_MA <- fitContinuous(tr, df_MA_fix, model = "OU")
+fit_MVA <- fitContinuous(tr, df_MVA_fix, model = "OU")
+
+#Now for adequacy
+
+#First make unit trees
+unit_MV <- make_unit_tree(fit_MV)
+unit_MA <- make_unit_tree(fit_MA)
+unit_MVA <- make_unit_tree(fit_MVA)
+
+#Now run arbutus
+MV_adequacy <- arbutus(fit_MV)
+MA_adequacy <- arbutus(fit_MA)
+MVA_adequacy <- arbutus(fit_MVA)
+
+
+#for comparison, will simulate a regular OU model and then calculate adequacy
+df_OU <- data.frame(rTraitCont(tr, model = "OU"))
+fit_OU <- fitContinuous(tr, df_OU, model = "OU")
+OU_adequacy <- arbutus(fit_OU)
