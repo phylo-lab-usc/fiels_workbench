254187d691f58cfaae5c311df4d01873c33a63e2
fieldima <60451947+fieldima@users.noreply.github.com>
2021-11-01 20:25:32 -0700

    Need to change which trees non-standard OU processes use. Also adding violin plots to show density.

--- Arbutus_Exploration_v2.R
+++ Arbutus_Exploration_v2.R
@@ -15,8 +15,13 @@ library(arbutus)
 tr <- sim.bdtree(n = 128)
 
 dat1 <- data.frame(tr$tip.label) %>% mutate(Reg = 1) %>% rename(Genus_species = tr.tip.label)
+
+#Use different trees for regular OU and OU variatns
 tree_OUwie <- tr
-tree_OUwie$node.label <- rep(1, 127)
+tree_OUwie$node.label <- c(rep(1, 64), rep(2, 63))
+
+tree_OU <- tr
+tree_OU$node.label <- rep(1,127)
 
 sim_and_fit_arbutus2 <- function (tree, dat, model) {
   ifelse(model == "OU", {
@@ -39,7 +44,7 @@ sim_and_fit_arbutus2 <- function (tree, dat, model) {
 }
 
 #run the sims
-OU_adequacy <- replicate(1000, sim_and_fit_arbutus2(tree_OUwie, dat1, model = "OU"))
+OU_adequacy <- replicate(1000, sim_and_fit_arbutus2(tree_OU, dat1, model = "OU"))
 MV_adequacy <- replicate(1000, sim_and_fit_arbutus2(tree_OUwie, dat1, model = "MV"))
 MA_adequacy <- replicate(1000, sim_and_fit_arbutus2(tree_OUwie, dat1, model = "MA"))
 MVA_adequacy <- replicate(1000, sim_and_fit_arbutus2(tree_OUwie, dat1, model = "MVA"))
@@ -66,4 +71,7 @@ data_plot <- fuse_df %>% pivot_longer(cols = c(-model), names_to = "test.stat") 
   ggplot(aes(x = value, fill = model)) + geom_boxplot() + facet_wrap(~test.stat)
 
 data_plot
+
+fuse_df %>% pivot_longer(cols = c(-model), names_to = "test.stat") %>%
+  ggplot(aes(y = value, x = model, fill = (model))) + geom_violin() + geom_boxplot(width = 0.5) + facet_wrap(~test.stat) + theme_bw()
 
