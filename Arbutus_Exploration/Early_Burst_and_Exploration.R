c1a837144c2fafbaff4ffa6f5c0cbff200b470f9
fieldima <fieldimayacyac@gmail.com>
2021-11-17 10:55:32 -0800

    Plots of three models (EB, BM, OU) to rescaled Early Burst trees

--- Arbutus_Exploration/Early_Burst_and_Exploration.R
+++ Arbutus_Exploration/Early_Burst_and_Exploration.R
@@ -25,9 +25,22 @@ plot_df %>% pivot_longer(cols = c(-model), names_to = "test.stat") %>%
 
 #Now to show Early Burst fit
 EB_EB <- readRDS("Arbutus_Exploration/RDSfiles/EB_fit_to_EB")
+EB_BM <- readRDS("Arbutus_Exploration/RDSfiles/EB_fit_to_BM")
+EB_OU <- readRDS("Arbutus_Exploration/RDSfiles/EB_fit_to_OU")
 
-#Change value to not a list.
-a <- EB_EB %>% select(-c(method, k)) %>% 
-  pivot_longer(cols = c(-alpha), names_to = "param") #%>%
-  ggplot(aes(x = value)) + geom_boxplot() + theme_classic()
+#Plot fit of different models to rescaled EB tree.
+EB_EB %>% select(-c(method, k)) %>% 
+  pivot_longer(cols = c(-alpha), names_to = "param") %>%
+  mutate(value = unlist(value)) %>%
+  ggplot(aes(x = value, fill = alpha)) + geom_boxplot() + facet_wrap(~param, scales = "free") + theme_classic() 
+
+EB_BM %>% select(-c(method, k)) %>% 
+  pivot_longer(cols = c(-alpha), names_to = "param") %>%
+  mutate(value = unlist(value)) %>%
+  ggplot(aes(x = value, fill = alpha)) + geom_boxplot() + facet_wrap(~param, scales = "free") + theme_classic() 
+
+EB_OU %>% select(-c(method, k)) %>% 
+  pivot_longer(cols = c(-alpha), names_to = "param") %>%
+  mutate(value = unlist(value)) %>%
+  ggplot(aes(x = value, fill = alpha)) + geom_boxplot() + facet_wrap(~param, scales = "free") + theme_classic() 
 
