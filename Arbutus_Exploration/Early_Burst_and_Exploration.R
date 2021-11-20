f8769e77a515bcbd384de5448deeac4443b644c9
fieldima <60451947+fieldima@users.noreply.github.com>
2021-11-20 10:40:55 -0800

    Updating markdown with new Early Burst

--- Arbutus_Exploration/Early_Burst_and_Exploration.R
+++ Arbutus_Exploration/Early_Burst_and_Exploration.R
@@ -5,23 +5,6 @@ library(geiger)
 library(OUwie)
 
 
-#Dataset loading for violin plots
-EB_violin <- readRDS("Arbutus_Exploration/RDSfiles/EB_data")
-others_violin <- readRDS("Arbutus_Exploration/RDSfiles/Exploration3_data")
-
-#Let's just use alpha = 1 for early burst
-EB_to_join <- EB_violin %>%
-  filter(alpha == "one") %>%
-  mutate(model = "EB") %>%
-  select(-alpha)
-
-plot_df <- EB_to_join %>% full_join(others_violin)
-
-#Composite violin plot
-plot_df %>% pivot_longer(cols = c(-model), names_to = "test.stat") %>%
-  ggplot(aes(y = value, x = model, fill = (model))) + 
-  geom_violin() + geom_boxplot(width = 0.5) + 
-  facet_wrap(~test.stat) + theme_bw()
 
 #Now to show Early Burst fit
 EB_EB <- readRDS("Arbutus_Exploration/RDSfiles/EB_fit_to_EB")
