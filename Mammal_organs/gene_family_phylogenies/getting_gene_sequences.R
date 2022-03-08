b6b91e60b61b57647251a77b86034097d113a7c5
fieldima <60451947+fieldima@users.noreply.github.com>
2022-03-07 23:53:59 -0800

    Getting gene sequences for amniote dataset

---
+++ Mammal_organs/gene_family_phylogenies/getting_gene_sequences.R
@@ -0,0 +1,51 @@
+#Get sequences for each gene ortholog group via biomart
+
+library(biomaRt)
+library(tidyverse)
+library(parallel)
+
+ortho <- read.delim("Mammal_organs/Supplementary_Data2/Ortho_1to1_AllSpecies.txt", sep = " ")
+seqlist <- ortho %>% t() %>% as.data.frame() %>% as.list()
+test <- seqlist[1:5]
+
+hsapiens <- useEnsembl(dataset = "hsapiens_gene_ensembl", biomart = "ensembl")
+chimps <- useEnsembl(dataset = "ptroglodytes_gene_ensembl", biomart = "ensembl")
+gorilla <- useEnsembl(dataset = "ggorilla_gene_ensembl", biomart = "ensembl")
+orangutan <- useEnsembl(dataset = "pabelii_gene_ensembl", biomart = "ensembl")
+macaque <- useEnsembl(dataset = "mmulatta_gene_ensembl", biomart = "ensembl")
+mouse <- useEnsembl(dataset = "mmusculus_gene_ensembl", biomart = "ensembl")
+opossum <- useEnsembl(dataset = "mdomestica_gene_ensembl", biomart = "ensembl")
+platypus <- useEnsembl(dataset = "oanatinus_gene_ensembl", biomart = "ensembl")
+chicken <- useEnsembl(dataset = "ggallus_gene_ensembl", biomart = "ensembl")
+
+get_sequences_list <- function( ortholist ){
+  df <- data.frame(cdna = "", ensembl_gene_id = "")
+  for(c in 1:length(ortholist)){
+    sequ <- getSequence(id = ortholist[[c]], type = "ensembl_gene_id", seqType = "cdna", mart = switch(c, 
+                                                                                                       "1" = hsapiens,
+                                                                                                       "2" = chimps,
+                                                                                                       "3" = gorilla,
+                                                                                                       "4" = orangutan,
+                                                                                                       "5" = macaque,
+                                                                                                       "6" = mouse,
+                                                                                                       "7" = opossum,
+                                                                                                       "8" = platypus,
+                                                                                                       "9" = chicken)) %>%
+      group_by(ensembl_gene_id)
+    if(! nrow(sequ) == 0){
+      add <- sequ %>% filter(cdna == max(cdna))
+      df <- full_join(df, sequ)
+    }
+  }
+    saveRDS(df, file = paste0("Mammal_organs/gene_family_phylogenies/tables/", ortholist[[1]]))
+    exportFASTA(df, file = paste0("Mammal_organs/gene_family_phylogenies/fasta_files/", ortholist[[1]]))
+  
+}
+
+system.time(
+  map(test, get_sequences_list)
+)
+
+system.time(
+  mclapply(test, get_sequences_list, mc.cores = 4)
+)
